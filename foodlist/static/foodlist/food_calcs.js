41946b47b5f08d0b291df7e4a0d280890fc11a0b
Vandal03 <abevanderpool228@gmail.com>
2021-08-18 23:30:23 -0400

    progress on javacript function to calculate cost

--- foodlist/static/foodlist/food_calcs.js
+++ foodlist/static/foodlist/food_calcs.js
@@ -7,9 +7,29 @@ function test() {
     }
 }
 
-function calculateCost(qty, cost) {
-    console.log(qty)
-    console.log(cost)
+
+
+
+
+var list = []
+function calculateCost(ingredient_cost, name, qty, food_cost) {
+
+    console.log(ingredient_cost, name, qty, food_cost)
+    
+    ingredient = {
+        ingredient_name : name,
+        cost : food_cost,
+        quantity : qty
+    }
+
+    list.push(ingredient)
+
+    // Does Object exist in array? .find(ingredient name)
+    // If yes, update qty
+    // If no, add new object (name, qty, cost)
+    // Iterate through list to calucatle cost (for each object cost += (qty * ing cost))
+    // update cost label and input value
+
     
 
 
